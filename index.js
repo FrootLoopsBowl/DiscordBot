622edfea2ff1019aafca22cf5911ecb73e1c6fcb
Froot <froott@protonmail.com>
2025-09-04 23:11:23 -0400

    Initial commit without secrets

---
+++ index.js
@@ -0,0 +1,46 @@
+const { token } = require('./config.json');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js')
+const fs = require('node:fs')
+const path = require('node:path')
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`Missing data or execute args in command => ${filePath}`);
+    }
+  }
+}
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error executing this command!', ephemeral: true });
+  }
+});
+
+
+client.login(token);
